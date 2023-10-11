8501109d6aa578329778674f1834116f1e417840
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-11 16:33:30 +0600

    charCodeAt

--- problem-1/index.js
+++ problem-1/index.js
@@ -1,5 +1,5 @@
-/*check two number and return true one of the number is 100 or sum is 100 */
-const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
+// problem-1 check two number and return true one of the number is 100 or sum is 100 
+/*const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
 console.log(isEqualTo100(100,0));
 console.log(isEqualTo100(0,100));
 
@@ -7,11 +7,21 @@ console.log(isEqualTo100(60,10));
 console.log(isEqualTo100(70,50));
 
 console.log(isEqualTo100(60,40));
-console.log(isEqualTo100(50,50));
+console.log(isEqualTo100(50,50));*/
 
+//problem-2 get the extension of a filename
+/*const getFilename = (str) => str.slice(str.lastIndexOf ('.'));
 
+console.log(getFilename("index.html"));
+console.log(getFilename("webpack.config.js"));*/
 
-
-
+//problem-3 replace every character in a given string with the character following it in the alphabet
+/*const moveCharasForward = (str) =>
+ str
+ .split('')
+ .map(char => String.fromCharCode(char.charCodeAt
+ (0) + 1))
+ .join('');
+ console.log(moveCharasForward('abcde'));*/
 
 
