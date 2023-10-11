7b057fd7443fea88061e8e219d2096b210b954ff
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-12 00:44:15 +0600

    even value given

--- problem-1/part-3.js
+++ problem-1/part-3.js
@@ -1,9 +1,24 @@
 //problem-1 find the number of even digits in an of integers
-
 /*const countEventNumber = (arr) => 
 arr.filter(num => num % 2 === 0).length;
 console.log(countEventNumber([1, 2, 3, 4, 5, 6]));
 console.log(countEventNumber([2, 2, 6, 9, 5, 6]));
 console.log(countEventNumber([1, 8, 3, 9, 4, 6]));*/
+
+
+//problem-2 find the number of even values up to a given number
+/*const countEventNumber = (arr) => 
+arr.filter(num => num % 2 === 0).length;
+
+const createArrayOfNumbers = (num) => {
+    const returnArray = [];
+    for(let i = 1; i <= num; i++){
+        returnArray.push(i);
+    }
+    return returnArray;
+};
+console.log(countEventNumber(createArrayOfNumbers(6)));
+console.log(countEventNumber(createArrayOfNumbers(10)));*/
+
 
 
