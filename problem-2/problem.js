a161134433d47d11537a86182243d2838b8fb819
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-11 22:32:39 +0600

    copy

--- problem-2/problem.js
+++ problem-2/problem.js
@@ -1,15 +1,39 @@
 //1. how can we swap values?
-let array =[1, 2, 3, 4, 5];
+//let array =[1, 2, 3, 4, 5];
 /*let temp = array[0];
 array[0] = array[4];
 array[4] = temp;
 console.log(array);*/
+
 //destructuring
-[array[0], array[4]] = [array[4], array[0]];
-console.log(array);
+/*[array[0], array[4]] = [array[4], array[0]];
+console.log(array);*/
+
+/*let a = 1;
+let b = 2;
+// [a,b] = [b,a]
+// console.log(a,b);
+
+//math
+b = a + (a = b) - b;
+console.log(a, b);*/
 
 
 //2. how to copy thing from clipboard
+function copyToClipBoard(str){
+    const element = document.createElement
+    ("textarea");
+    element.value = str;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand("copy");
+    document.body.removeChild(element);
+}
+function handelClick(){
+    let text = document.querySelector("#text");
+    copyToClipBoard(text.innerHTML)
+}
+
 //3. destructuring aliases
 //4. get value as data type
 //5. remove duplicate from array
