bb9372eda0f9df7db2e22c255bb559fa6deab9ce
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-12 18:31:45 +0600

    comma-separated

--- problem-1/part-4.js
+++ problem-1/part-4.js
@@ -1,12 +1,32 @@
 //problem-1 report if it is a leap year
-const isLeapYear = (year) => year % 4 === 0;
+/*const isLeapYear = (year) => year % 4 === 0;
 console.log(isLeapYear(2015));
-console.log(isLeapYear(2016));
+console.log(isLeapYear(2016));*/
 
 
+//problem-2 compare teo objects to determine (additional properties)
+/*const objA = { a: 1, b: 2, c: 1};
+const objB = { a: 1, b: 1, c: 1};
+const objC = { a: 1, b: 2, c: 1};
+
+const objectsEqual =(a, b) => {
+    Object.keys(a).every(key => b[key]);
+    // console.log(Object.keys(a));
+    // console.log(Object.keys(b));
+};
+console.log(objectsEqual(objA, objB));*/
 
 
+//problem-3 program to convert a comma-separated values (CSV) string to a 2D array. A new line indicates a new row in the array.
+/*const parseCSV = (csvString) => 
+    csvString.split('\n').map(row => row.split(','));
+const str = `abc, def, ghi
+  jkl, mno, pqr
+  stu, vwx, yza`;
+console.log(parseCSV(str));*/
 
+
+//problem-4  program to generate a random hexadecimal color code.
 
 
 
