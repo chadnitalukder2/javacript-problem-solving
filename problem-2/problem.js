d56e937546c0953ace2630c8a1d57f89db08d70a
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-13 09:49:29 +0600

    comma

--- problem-2/problem.js
+++ problem-2/problem.js
@@ -60,7 +60,18 @@ console.log(hasSameElements([1, 2, 6], [1, 2, 6]));
 console.log(hasSameElements([1, 6, 5], [1, 2, 6]));*/
 
 //7. shuffling an array
-const numbers = [1, 2, 3, 4, 5, 6];
-console.log(numbers.sort(() => Math.random() - 0.5));
+/*const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers.sort(() => Math.random() - 0.5));*/
 
 //8. using comma operator
+/*let x = 1;
+x =(x++, x);
+console.log(x);
+
+let y =(2,3)
+console.log(y);*/
+
+/*let a = [[1, 2, 3, 4 ],[3, 4, 5], [5, 6], [7]];
+for ( let i = 0, j = 3; i <= 3; i++, j--){
+    console.log("a [" + i + "][" + j + "] = " + a[i][j]);
+}*/
